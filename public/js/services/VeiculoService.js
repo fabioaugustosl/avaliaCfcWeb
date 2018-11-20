eeb9a61f0cd2b4899012db5ae8bee2812e5ebac4
Fabio Lopes <fabio.lopes@gmail.com>
2018-11-20 13:44:02 -0200

    .

--- public/js/services/VeiculoService.js
+++ public/js/services/VeiculoService.js
@@ -2,7 +2,7 @@
 apoioApp.factory('veiculoService', function($http, $log){
 	
 	//var urlPadrao = 'http://localhost:3000'; 
-	var urlPadrao = 'http://veiculo.virtz.com.br:3000';
+	var urlPadrao = 'http://cfc.virtz.com.br:3000';
 
 	var urlVeiculo = urlPadrao+'/api/veiculo/v1/';
 	
